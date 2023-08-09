e176d630e2e426893fe949b0f320b6c26c353583
Glitch <glitch.txs@gmail.com>
2023-08-09 14:26:42 -0300

    sepolia test

--- walletconnect/config.ts
+++ walletconnect/config.ts
@@ -5,6 +5,7 @@ if(!process.env.NEXT_PUBLIC_PROJECT_ID) throw new Error("Project ID Missing")
 
 export const config = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+  chains:[11155111],
   optionalChains:[1, 5, 56, 42161],
   showQrModal: true
 } satisfies EthereumProviderOptions
