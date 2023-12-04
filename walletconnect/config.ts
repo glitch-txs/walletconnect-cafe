6897e6611bdafb964fbd1a098d891937c12c7807
Glitch <glitch.txs@gmail.com>
2023-12-04 19:12:41 -0300

    ronin

--- walletconnect/config.ts
+++ walletconnect/config.ts
@@ -1,11 +1,8 @@
 //@ts-ignore
 import { EthereumProviderOptions } from '@walletconnect/ethereum-provider/dist/types/EthereumProvider'
 
-if(!process.env.NEXT_PUBLIC_PROJECT_ID) throw new Error("Project ID Missing")
-
 export const config = {
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
-  chains: [1],
-  optionalChains:[1 , 137],
+  projectId: "bd4997ce3ede37c95770ba10a3804dad",
+  optionalChains:[2020 ,2021],
   showQrModal: true
 } satisfies EthereumProviderOptions
